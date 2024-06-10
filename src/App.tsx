bccfd1c7b33debb6ae34a7cff0018cf29b6ec71b
Ori <ori@example.com>
2024-06-10 19:09:20 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import ContactMe from "./components/ContactMe";
-import Users from "./components/Users";
-import { UserType } from "./types/user-type";
-
-
-const users : UserType[] = [
-    { name: 'David', age: 5 },
-    { name: 'Moshe', age: 50 },
-    { name: 'Yakov', age: 55 },
-    { name: 'Yoni', age: 500 },
-    { name: 'Zerach', age: -5 },
-]
 
 function App() {
     return (
         <div>
             <h1> Hello React-Typescript template</h1>
-            <div>
-                <Users users={users}/>
-                <br/>
-                <ContactMe/>
-            </div>
         </div>
     );
 }
