069b43d25b32ab5118c622ec41d6f26db0910bd6
ori_b <ori@ori>
2024-08-16 06:41:37 +0300

    fix fetch issue by adding content-type

--- src/client/orderApi.ts
+++ src/client/orderApi.ts
@@ -12,12 +12,20 @@ export async function sendOrder(order: any) {
     for (const pid of Object.keys( order)) {
         orderToSend.push({ "productId": pid, "quantity": order[pid] });
     }
+
+
+    
     const data = {"productList": orderToSend, comments: "Don't add sugar",}
     const url = "http://localhost:4000/api/v1/order";
     const res = await fetch(url, { method: 'POST', 
                                     body: JSON.stringify(data),
-                                   headers: { Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyV2l0aG91dFBhc3N3b3JkIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkRhdmlkIiwiZW1haWwiOiJkYXZpZEBnbWFpbC5jb20iLCJpc0FkbWluIjowLCJ0b2tlbiI6IiJ9LCJpYXQiOjE3MjM3MzkwNjZ9.wjMIRFXJnu3XGDJVIT8JNwtU_xod5tv0IS49ru4hbGo" } });
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyV2l0aG91dFBhc3N3b3JkIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkRhdmlkIiwiZW1haWwiOiJkYXZpZEBnbWFpbC5jb20iLCJpc0FkbWluIjowLCJ0b2tlbiI6IiJ9LCJpYXQiOjE3MjM3MzkwNjZ9.wjMIRFXJnu3XGDJVIT8JNwtU_xod5tv0IS49ru4hbGo"
+        }
+    });
     
-    console.log(res);
+    const resJ = await res.json();
+    console.log(resJ);
     
 }
--- src/client/productApi.ts
+++ src/client/productApi.ts
@@ -1,6 +1,6 @@
 export async function getAllProducts() {
     const url = "http://localhost:4000/api/v1/products";
     const res = await fetch(url, {headers: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyV2l0aG91dFBhc3N3b3JkIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkRhdmlkIiwiZW1haWwiOiJkYXZpZEBnbWFpbC5jb20iLCJpc0FkbWluIjowLCJ0b2tlbiI6IiJ9LCJpYXQiOjE3MjM3MzkwNjZ9.wjMIRFXJnu3XGDJVIT8JNwtU_xod5tv0IS49ru4hbGo"}});
-    const resJ = await res.json();        
-    return resJ;
+    const resJ = await res.json();          
+    return resJ; 
 }
