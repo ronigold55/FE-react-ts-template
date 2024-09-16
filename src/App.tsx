a391b5420798e83b902f008d40c7c608ad97848d
ronigold55 <sargiel55@gmail.com>
2024-09-16 19:16:23 +0300

    react play

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,7 @@ function App() {
     }
 
 
-    // const fetchParks = asunc ()=> {
-    //     if (park){
-    //         setParks (park)
-    //     }
-    // }
+    
 
     return (
         <div>
