45b63774227769cc74938f9c872e2f1758808c69
ronigold55 <sargiel55@gmail.com>
2024-10-07 19:05:45 +0300

    websocket 7.10.24

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express"
-import handleSocketIO from "./sevices/socket-service";
+import handleSocketIO from "./services/socket-Services";
 
 const server = express();
 
